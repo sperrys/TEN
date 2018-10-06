399f60ed3a683b6623e26189dd33b04599791e0a
mgomez05 <gomezruss7@gmail.com>
2018-10-06 12:50:51 -0400

    Moved sell page react code into index.js in SellPage folder

--- frontend/sellPage/my-app/src/index.js
+++ frontend/sellPage/my-app/src/index.js
@@ -3,67 +3,99 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class NameForm extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {name: 'Tony Monacle',
+		            isbn: 'eg. 9781234567890',
+		            classID: 'eg. 0022',
+		            textbookTitle: "eg. An Invitation to Spanish (student edition)",
+		            editionNumber: "eg. 9",
+		            price: "eg. 33 ",
+		            email: "eg. example@mail.com",
+		            phoneNumber: "eg. 123-456-7890",
+		            venmoID: "eg. @jumboKid22"            
+	};
+
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      const target = event.target;
+      const name   = target.name;
+      const value  = target.value;
+	  this.setState({name: value});
+    }
+  
+
+    handleSubmit(event) {
+	    alert('A name was submitted: ' + this.state.name);
+	    event.preventDefault();
+    }
+
     render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
+	return (
+		<form onSubmit={this.handleSubmit}>
+        <label>
+		Name:
+		<input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		ISBN:
+		<input name="isbn" type="text" value={this.state.isbn} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Class ID:
+		<input name="classID" type="text" value={this.state.classID} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Textbook Title:
+		<input name="textbookTitle" type="text" value={this.state.textbookTitle} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Edition Number:
+		<input name="editionNumber" type="number" value={this.state.editionNumber} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Price:
+		<input name="price" type="number" value={this.state.price} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Email:
+		<input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Phone Number:
+		<input name="phoneNumber" type="text" value={this.state.phoneNumber} onChange={this.handleChange} />
+        </label>
+        
+        <label>
+		Venmo ID:
+		<input name="venmoID" type="text" value={this.state.venmoID} onChange={this.handleChange} />
+        </label>
+        
+        
+        
+        
+        
+        
+        
+        
+        <input type="submit" value="Submit" />
+      </form>
+		);
     }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+}
+
+ReactDOM.render(
+		<NameForm />,
+		document.getElementById('root')
+		);
