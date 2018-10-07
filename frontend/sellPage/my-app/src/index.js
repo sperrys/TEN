a15aef9c91c2a48e85d9c34e389fc34a4569c27b
mgomez05 <gomezruss7@gmail.com>
2018-10-07 12:00:18 -0400

    Started lifting labels out as controlled components

--- frontend/sellPage/my-app/src/index.js
+++ frontend/sellPage/my-app/src/index.js
@@ -6,16 +6,22 @@ import './index.css';
 class SellPage extends React.Component {
     constructor(props) {
 	super(props);
-	this.state = {name: 'Tony Monacle',
-		            isbn: 'eg. 9781234567890',
-		            classID: 'eg. 0022',
-		            textbookTitle: "eg. An Invitation to Spanish (student edition)",
-		            editionNumber: "eg. 9",
-		            price: "eg. 33 ",
-		            email: "eg. example@mail.com",
-		            phoneNumber: "eg. 123-456-7890",
-		            venmoID: "eg. @jumboKid22"            
-	};
+
+	var labelNames = {name: 'Tony Monacle',
+					  isbn: 'eg. 9781234567890',
+					  classID: 'eg. 0022',
+					  textbookTitle: "eg. An Invitation to Spanish (student edition)",
+					  editionNumber: "eg. 9",
+					  price: "eg. 33 ",
+					  email: "eg. example@mail.com",
+				      phoneNumber: "eg. 123-456-7890",
+				      venmoID: "eg. @jumboKid22"}
+
+	var numLabels = Object.getOwnPropertyNames(labelNames).length;
+
+	this.state = {labelNames: labelNames,
+				   numLabels: numLabels,
+				   inputLabels: Array(numLabels).fill(null)};
 
 	this.handleChange = this.handleChange.bind(this);
 	this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,80 +30,59 @@ class SellPage extends React.Component {
     handleChange(event) {
       const target = event.target;
       const name   = target.name;
-      const value  = target.value;
+	  const value  = target.value;
 	  this.setState({name: value});
     }
   
 
     handleSubmit(event) {
-	    alert('A name was submitted: ' + this.state.name);
+		alert('A name was submitted: ' + this.state.name);
+
 	    event.preventDefault();
-    }
+	}
+	
+	renderLabel(name, type, value)
+	{
+		return (
+			<Label
+			  name={name}
+			  type={type}
+			  value={value}
+			  onChange={() => this.handleChange()}
+			/>
+		  );
+	}
 
     render() {
 	return (
-
-       
-
 		<form onSubmit={this.handleSubmit}>
         <p>Textbook Exchange Network Sell Page</p>
-        <label>
-		Name:
-		<input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		ISBN:
-		<input name="isbn" type="text" value={this.state.isbn} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Class ID:
-		<input name="classID" type="text" value={this.state.classID} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Textbook Title:
-		<input name="textbookTitle" type="text" value={this.state.textbookTitle} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Edition Number:
-		<input name="editionNumber" type="number" value={this.state.editionNumber} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Price:
-		<input name="price" type="number" value={this.state.price} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Email:
-		<input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Phone Number:
-		<input name="phoneNumber" type="text" value={this.state.phoneNumber} onChange={this.handleChange} />
-        </label>
-        
-        <label>
-		Venmo ID:
-		<input name="venmoID" type="text" value={this.state.venmoID} onChange={this.handleChange} />
-        </label>
-        
-        
-        
-        
-        
-        
-        
-        
+
+		{this.renderLabel("name", 		   "text",   this.state.inputLabels.name)}
+		{this.renderLabel("isbn", 		   "text",   this.state.inputLabels.isbn)}
+		{this.renderLabel("classID", 	   "text", 	 this.state.inputLabels.classID)}
+		{this.renderLabel("textbookTitle", "text",   this.state.inputLabels.textbookTitle)}
+		{this.renderLabel("editionNumber", "number", this.state.inputLabels.editionNumber)}
+		{this.renderLabel("price", 		   "number", this.state.inputLabels.price)}
+		{this.renderLabel("email", 		   "text",   this.state.inputLabels.email)}
+		{this.renderLabel("phoneNumber",   "text",   this.state.inputLabels.phoneNumber)}
+		{this.renderLabel("venmoID", 	   "text",   this.state.inputLabels.venmoID)}
+            
         <input type="submit" value="Submit" />
       </form>
 		);
     }
 }
+
+class Label extends React.Component {
+	render() {
+	  return (
+		<label className="label">
+			<input name="a label" type="text" value="something"/>
+		</label>
+	  );
+	}
+  }  
 
 ReactDOM.render(
 		<SellPage />,
