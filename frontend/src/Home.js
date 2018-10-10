4d0d78c9ab5c2842cbd14493f6384a4fc6457460
zallenz <allen.zhou@tufts.edu>
2018-10-10 12:13:45 -0400

    Home page added

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,10 +6,16 @@ import NavigationBar from './NavigationBar.js'
 class App extends Component {
     render() {
         return (
-            <div>
-                <NavigationBar />
-                <div>This is our home page!</div>
-            </div>
+        	<div>
+        		<NavigationBar />
+		      <div class="vertcenter">
+		          <h1 id= "title"> Tufts Textbook Exchange </h1>
+		          <div class="horizflex">
+		              <button><a href="browse.html">Browse</a></button>
+		              <button><a href="studentsell.html">Sell</a></button>      
+		          </div>
+		      </div>
+	      </div>
         );
     }
 }
