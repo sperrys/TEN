9230fa618304035be2d87f427d38e466fe4439bf
mgomez05 <gomezruss7@gmail.com>
2018-10-13 11:48:37 -0400

    Changed state for labels to array and made dynamic rendering

--- frontend/sellPage/my-app/src/index.js
+++ frontend/sellPage/my-app/src/index.js
@@ -8,16 +8,16 @@ class SellPage extends React.Component {
     constructor(props) {
 	super(props);
 							
-	this.state = {labelNames: {
-		name: 'Tony Monacle',
-		isbn: 'eg. 9781234567890',
-		classID: 'eg. 0022',
-		textbookTitle: "eg. An Invitation to Spanish (student edition)",
-		editionNumber: "eg. 9",
-		price: "eg. 33 ",
-		email: "eg. example@mail.com",
-		phoneNumber: "eg. 123-456-7890",
-		venmoID: "eg. @jumboKid22"}
+	this.state = {labelNames: 
+		[{name: 'name', value: '', placeholder: 'eg. Tony Monacle'},
+		 {name: 'isbn', value: '', placeholder: 'eg. 9781234567890'},
+		 {name: 'classID', value: '', placeholder: 'eg. 0022'},
+		 {name: 'Textbook Title', value: '', placeholder: 'eg. An Invitation to Spanish (student edition)'},
+		 {name: 'Edition Number', value: '', placeholder: 'eg. 9'},
+		 {name: 'Price', value: '', placeholder: 'eg. 33'},
+		 {name: 'Email', value: '', placeholder: 'eg. example@mail.com'},
+		 {name: 'Phone Number', value: '', placeholder: 'eg. 123-456-7890'},
+		 {name: 'Venmo ID', value: '', placeholder: 'eg. @jumboKid22'}]
 	};
 
 
@@ -35,9 +35,20 @@ class SellPage extends React.Component {
 			console.log("name: " + name + ", value: " + value);
 			
 			// TODO could make labels immutable
-			var labels = this.state.labelNames
+			var labels = this.state.labelNames;
 
-			labels[name] = value;
+			for (var i = 0; i < labels.length; i++)
+			{
+				var label = labels[i];
+
+				if (label.name === name)
+				{
+					labels[i].value = value;
+					break;
+				}
+			}
+
+			//labels[name] = value;
 			console.log("new labels:")
 			console.log(labels);
 
@@ -52,23 +63,16 @@ class SellPage extends React.Component {
 		
 
 		const labelNames = this.state.labelNames;
-		const labelKeys = Object.getOwnPropertyNames(labelNames);
-
-		//console.log("label keys");
-		//console.log(labelKeys);
-		
+		//const labelKeys = Object.getOwnPropertyNames(labelNames);
 
 		var submitArray = [];
 
-		for (var i = 0; i < labelKeys.length; i++) 
+		for (var i = 0; i < labelNames.length; i++) 
 		{
-			var labelKey = labelKeys[i];
+			var label = labelNames[i];
 
-			//console.log("label key" );
-			//console.log(labelKey);
 			var labelObject = {};
-			labelObject[labelKey] = labelNames[labelKey];
-			//console.log(labelObject);
+			labelObject[label.name] = label.value;
 			submitArray.push(labelObject);
 		}
 
@@ -79,7 +83,7 @@ class SellPage extends React.Component {
 	    event.preventDefault();
 	}
 	
-	renderLabel(name, type, value)
+	renderLabel(name, type, value, placeholder)
 	{
 		return (
 			<Label
@@ -87,6 +91,7 @@ class SellPage extends React.Component {
 			  type={type}
 			  value={value}
 			  onChange={(event) => this.handleChange(event)}
+			  placeholder={placeholder}
 			/>
 		  );
 	}
@@ -106,18 +111,25 @@ class SellPage extends React.Component {
 		); 
 		console.log(Test);*/
 
+
+
 		return(
-		<>
-		{this.renderLabel("name", 		     "text",   this.state.labelNames.name)}
-		{this.renderLabel("isbn", 		     "text",   this.state.labelNames.isbn)}
-		{this.renderLabel("classID", 	     "text", 	 this.state.labelNames.classID)}
-		{this.renderLabel("textbookTitle", "text",   this.state.labelNames.textbookTitle)}
-		{this.renderLabel("editionNumber", "number", this.state.labelNames.editionNumber)}
-		{this.renderLabel("price", 		     "number", this.state.labelNames.price)}
-		{this.renderLabel("email", 		     "text",   this.state.labelNames.email)}
-		{this.renderLabel("phoneNumber",   "text",  this.state.labelNames.phoneNumber)}
-		{this.renderLabel("venmoID", 	    "text",    this.state.labelNames.venmoID)}
-		</>
+			<div>
+				{this.state.labelNames.map((label, index) =>
+				(this.renderLabel(label.name, "text", label.value, label.placeholder)))
+				}
+			</div>
+		/*<>
+		{this.renderLabel("name", 		     "text",   "", this.state.labelNames.name)}
+		{this.renderLabel("isbn", 		     "text",   "", this.state.labelNames.isbn)}
+		{this.renderLabel("classID", 	     "text",   "", this.state.labelNames.classID)}
+		{this.renderLabel("textbookTitle",   "text",   "", this.state.labelNames.textbookTitle)}
+		{this.renderLabel("editionNumber",   "number", "", this.state.labelNames.editionNumber)}
+		{this.renderLabel("price", 		     "number", "", this.state.labelNames.price)}
+		{this.renderLabel("email", 		     "text",   "", this.state.labelNames.email)}
+		{this.renderLabel("phoneNumber",     "text",   "", this.state.labelNames.phoneNumber)}
+		{this.renderLabel("venmoID", 	     "text",   "", this.state.labelNames.venmoID)}
+		</>*/
 		)       
 	}
 
@@ -159,10 +171,12 @@ class Label extends React.Component {
 	  return (
 		<label className="label">
 			{this.props.name}
-			<input name={this.props.name} 
-					type={this.props.type}
-					onChange={this.props.onChange}  
-					value={this.props.value}
+			<input name=        {this.props.name} 
+					type=       {this.props.type}
+					onChange=   {this.props.onChange}  
+					placeholder={this.props.placeholder}
+					value=      {this.props.value}
+					
 					   />
 		</label>
 	  );
