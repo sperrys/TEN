55fe2105a5a61ca972185f0eca0d1621c4624648
Barry Eom <31739208+barieom@users.noreply.github.com>
2018-10-17 12:49:13 -0400

    two tables for user

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 
-# Create models here!
+# Two tables for scalability purposes
+# Textbook Table:
 class textbook(models.Model):
-    date = models.DateField(max_length=50)
+    email = models.CharField(max_length=100)
+    date_added = models.DateField(max_length=50)
+    date_removed = models.DateField(max_length=50)
     isbn = models.PositiveIntegerField()
     subject = models.CharField(max_length=5)
     class_id = models.PositiveIntegerField()
     title = models.CharField(max_length=100)
     edition = models.CharField(max_length=20)
     price = models.PositiveIntegerField()
+    donate = models.BooleanField()
+    sold = models.BooleanField()
+    venmo_yes = models.BooleanField() #if no, cash
+    paid = models.BooleanField()
+    book_id = models.PostiveIntegerField()
+
+
+# User Table:
+class user(models.Model):
+    name  = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     phone = models.PostiveIntegerField()
     venmo = models.CharField(max_length=50)
-    donate = models.BooleanField()
-    book_id = models.PostiveIntegerField()
-    sold = models.BooleanField()
-    buyer_payment = models.CharField(max_length=50)
-    paid = models.BooleanField()
