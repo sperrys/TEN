44299a5f3b638bb3bd7f295e3371f318dd4569b0
Barry Eom <31739208+barieom@users.noreply.github.com>
2018-10-17 12:55:08 -0400

    two tables for user and textbextbook complete

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,20 +3,24 @@ from django.db import models
 # Two tables for scalability purposes
 # Textbook Table:
 class textbook(models.Model):
-    email = models.CharField(max_length=100)
-    date_added = models.DateField(max_length=50)
-    date_removed = models.DateField(max_length=50)
-    isbn = models.PositiveIntegerField()
-    subject = models.CharField(max_length=5)
-    class_id = models.PositiveIntegerField()
-    title = models.CharField(max_length=100)
-    edition = models.CharField(max_length=20)
-    price = models.PositiveIntegerField()
-    donate = models.BooleanField()
-    sold = models.BooleanField()
-    venmo_yes = models.BooleanField() #if no, cash
-    paid = models.BooleanField()
-    book_id = models.PostiveIntegerField()
+    # Filled during book posting
+    book_id      = models.PostiveIntegerField()
+    email_seller = models.CharField(max_length=100)
+    date_added   = models.DateField(max_length=50)
+    isbn         = models.PositiveIntegerField()
+    subject      = models.CharField(max_length=5)
+    class_id     = models.PositiveIntegerField()
+    title        = models.CharField(max_length=100)
+    edition      = models.CharField(max_length=20)
+    price        = models.PositiveIntegerField()
+    donate       = models.BooleanField()
+
+    # Completed during buying process
+    email_buyer   = models.models.CharField(max_length=100)
+    sold          = models.BooleanField()
+    venmo_payment = models.BooleanField() #if no, cash
+    paid          = models.BooleanField()
+    date_removed  = models.DateField(max_length=50)
 
 
 # User Table:
