3dbafb8dca1f3d6d93bfe46b783112f36878758b
mgomez05 <gomezruss7@gmail.com>
2018-10-20 11:39:13 -0400

    Added list attribute to field labels, and added options for subject field

--- frontend/sellPage/my-app/src/index.js
+++ frontend/sellPage/my-app/src/index.js
@@ -9,16 +9,16 @@ class SellPage extends React.Component {
 	super(props);
 							
 	this.state = {labelNames: 
-		[{name: 'Name',           value: '', type: "text",   placeholder: 'eg. Tony Monacle'},
-		 {name: 'ISBN',           value: '', type: "number", placeholder: 'eg. 9781234567890'},
-		 {name: 'Subject', 		  value: '', type: "text",   placeholder: 'eg. SPN'},
-		 {name: 'Class ID',       value: '', type: "number", placeholder: 'eg. 0022'},
-		 {name: 'Textbook Title', value: '', type: "text",   placeholder: 'eg. An Invitation to Spanish (student edition)'},
-		 {name: 'Edition Number', value: '', type: "number", placeholder: 'eg. 9'},
-		 {name: 'Price', 		  value: '', type: "number", placeholder: 'eg. 33'},
-		 {name: 'Email', 		  value: '', type: "text",   placeholder: 'eg. example@mail.com'},
-		 {name: 'Phone Number',   value: '', type: "text",   placeholder: 'eg. 123-456-7890'},
-		 {name: 'Venmo ID', 	  value: '', type: "text",   placeholder: 'eg. @jumboKid22'}]
+		[{name: 'Name',           value: '', type: "text",   placeholder: 'eg. Tony Monacle', 								list: null},
+		 {name: 'ISBN',           value: '', type: "number", placeholder: 'eg. 9781234567890', 								list: null},
+		 {name: 'Subject', 		  value: '', type: "text",   placeholder: 'eg. SPN', 										list: "courses"},
+		 {name: 'Class ID',       value: '', type: "number", placeholder: 'eg. 0022', 										list: null},
+		 {name: 'Textbook Title', value: '', type: "text",   placeholder: 'eg. An Invitation to Spanish (student edition)', list: null},
+		 {name: 'Edition Number', value: '', type: "number", placeholder: 'eg. 9', 										    list: null},
+		 {name: 'Price', 		  value: '', type: "number", placeholder: 'eg. 33', 			  							list: null},
+		 {name: 'Email', 		  value: '', type: "text",   placeholder: 'eg. example@mail.com', 						    list: null},
+		 {name: 'Phone Number',   value: '', type: "text",   placeholder: 'eg. 123-456-7890',     							list: null},
+		 {name: 'Venmo ID', 	  value: '', type: "text",   placeholder: 'eg. @jumboKid22',      							list: null}]
 	};
 
 
@@ -89,11 +89,11 @@ class SellPage extends React.Component {
 			<option value = "AAST">Asian American Studies</option>
 			<option value = "ACL">All College</option>
 			<option value = "AFR">Africana Studies</option>
-		<option value = "AMER">American Studies</option>
+		    <option value = "AMER">American Studies</option>
 		</datalist>)
 	}
 
-	renderLabel(name, type, value, placeholder, key)
+	renderLabel(name, type, value, placeholder, key, list)
 	{
 		return (
 			<Label
@@ -103,6 +103,7 @@ class SellPage extends React.Component {
 			  onChange={(event) => this.handleChange(event)}
 			  placeholder={placeholder}
 			  key={key}
+			  list={list}
 			/>
 		  );
 	}
@@ -113,7 +114,7 @@ class SellPage extends React.Component {
 		return(
 			<div>
 				{this.state.labelNames.map((label, index) =>
-				(this.renderLabel(label.name, label.type, label.value, label.placeholder, label.name)))
+				(this.renderLabel(label.name, label.type, label.value, label.placeholder, label.name, label.list)))
 				}
 			</div>
 
@@ -124,13 +125,22 @@ class SellPage extends React.Component {
 
 	
 	return (
+
+		
+
 		<form onSubmit={this.handleSubmit}>
         <p>Textbook Exchange Network Sell Page</p>
 
 				{this._renderLabels()}
+
+				{this.getCourseList()}
 		
         <input type="submit" value="Submit" />
+
+		
+
       </form>
+	  
 		);
     }
 }
@@ -158,11 +168,12 @@ class Label extends React.Component {
 	  return (
 		<label className="label">
 			{this.props.name}
-			<input name=        {this.props.name} 
+			<input  name=       {this.props.name} 
 					type=       {this.props.type}
 					onChange=   {this.props.onChange}  
 					placeholder={this.props.placeholder}
 					value=      {this.props.value}
+					list= 		{this.props.list}
 					
 					   />
 		</label>
