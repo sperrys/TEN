1497581b63251af7e6ec248512d9fbd391f565de
Juliet Yue <julietyue@Juliets-MacBook-Pro-3.local>
2018-10-21 02:34:02 -0400

    improved database

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class textbook(models.Model):
     # Filled during book posting
     book_id      = models.PostiveIntegerField()
-    email_seller = models.CharField(max_length=100)
     date_added   = models.DateField(max_length=50)
     isbn         = models.PositiveIntegerField()
     subject      = models.CharField(max_length=5)
@@ -14,18 +13,28 @@ class textbook(models.Model):
     edition      = models.CharField(max_length=20)
     price        = models.PositiveIntegerField()
     donate       = models.BooleanField()
+    present      = models.BooleanField()
 
     # Completed during buying process
-    email_buyer   = models.models.CharField(max_length=100)
     sold          = models.BooleanField()
-    venmo_payment = models.BooleanField() #if no, cash
     paid          = models.BooleanField()
     date_removed  = models.DateField(max_length=50)
 
+    #buyer/seller data, reference to user table for consistancy
+    seller_id = models.ForeignKey('user', on_delete=models.PROTECT)
+    buyer_id = models.ForeignKey('user', on_delete=models.PROTECT)
+
+    def is_valid(self):
+        if not present:
+            #compare time
 
 # User Table:
 class user(models.Model):
+    user_id = models.PostiveIntegerField(primary_key=True)
     name  = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
     phone = models.PostiveIntegerField()
     venmo = models.CharField(max_length=50)
+    def __str__(self):
+        return self.user_id
+
