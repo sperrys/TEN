d4b2513beb12dad86dfbe0bb6950d43430c83edb
Juliet Yue <julietyue@Juliets-MacBook-Pro-3.local>
2018-10-22 12:38:51 -0400

    update database

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,19 +2,18 @@ from django.db import models
 
 # Two tables for scalability purposes
 # Textbook Table:
-class textbook(models.Model):
+class ten(models.Model):
     # Filled during book posting
-    book_id      = models.PostiveIntegerField()
+    post_id      = models.PostiveIntegerField()
     date_added   = models.DateField(max_length=50)
-    isbn         = models.PositiveIntegerField()
     subject      = models.CharField(max_length=5)
     class_id     = models.PositiveIntegerField()
-    title        = models.CharField(max_length=100)
-    edition      = models.CharField(max_length=20)
     price        = models.PositiveIntegerField()
     donate       = models.BooleanField()
     present      = models.BooleanField()
-
+    isbn         = models.PostiveIntegerField()
+    title        = models.CharField(max_length=100)
+    edition      = models.CharField(max_length=20)
     # Completed during buying process
     sold          = models.BooleanField()
     paid          = models.BooleanField()
@@ -35,7 +34,7 @@ class user(models.Model):
     email = models.CharField(max_length=100, unique=True)
     phone = models.PostiveIntegerField()
     venmo = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.user_id
 
