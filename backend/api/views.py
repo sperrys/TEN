22aa23d99a0e3fd6b133fb9c01b4a4418a6ed6c4
Juliet Yue <julietyue@Juliets-MacBook-Pro-3.local>
2018-10-29 13:14:15 -0400

    setup

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,31 +1,14 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-
-
-# Create your views here.
-
-from .models import *
-from .serializers import *
-
-from rest_framework.views import APIView
-from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework import generics
 
+from .models import *
+
+from .serializers import *
+
+
 # Create views here!
-class ListBookRaw(APIView):
+class ListBookRaw(generics.ListCreateAPIView):
+    queryset = Textbook.objects.all()
+    serializer_class = SellSerializer
 
-    def get(self, request, format=None):
-        """
-        Return a list of all books.
-        """
-        queryset = Textbook.objects.all()
-        serializer = SellSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        """
-        How might a post request work?
-        """
-        pass
 
--- backend/src/settings/development.py
+++ backend/src/settings/development.py
@@ -1,17 +1,41 @@
-# settings associated with someone who is working locally and trying stuff out
-
+#
+# These are settings for local development
+#
+from rest_framework.settings import api_settings
 from .base import *
 
 DEBUG = True
+ALLOWED_HOSTS = ['localhost']
 
-INSTALLED_APPS += [
-    'debug_toolbar',
-]
+# It's okay to have secret key in plain text since it is
+# different then the production environment key
 
-MIDDLEWARE += ['debug_toolbar.middlewar.DebugToolbarMiddleware', ]
+SECRET_KEY = 'm&%ij7#cpxk$p2w(@%u#7s)_$@!%)#fgp&^7q&0id1b4(fg(o&'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-DEBUG_TOLLBAR_CONFIG = {
-    'JQUERY_URL': '',
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# The necessary information for connecting to the
+# the postgres database running on our local machine
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangotutorial',
+        'USER': 'djangotutorial',
+        'PASSWORD': 'supersecret',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
+DEFAULT_RENDERER_CLASSES = DEFAULT_RENDERER_CLASSES + (
+        'rest_framework.renderers.BrowsableAPIRenderer',
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
 }
