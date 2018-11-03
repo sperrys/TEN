39dd1c55894ba0160b6125e0ee0df346cd113f84
radhikajoshi99 <43527996+radhikajoshi99@users.noreply.github.com>
2018-11-03 11:40:04 -0400

    Started work on confirmation page

--- frontend/src/Components/Sell/confirmationPage.js
+++ frontend/src/Components/Sell/confirmationPage.js
@@ -1,102 +1,63 @@
-class NameForm extends React.Component {
-    constructor(props) {
-	super(props);
-	this.state = {name: 'Tony Monacle',
-		      isbn: 'eg. 9781234567890',
-		      classID: 'eg. 0022',
-		      textbookTitle: "eg. An Invitation to Spanish (student edition)",
-		      editionNumber: "eg. 9",
-		      price: "eg. 33 ",
-		      email: "eg. example@mail.com",
-		      phoneNumber: "eg. 123-456-7890",
-		      venmoID: "eg. @jumboKid22"
-                  
-	};
+import React, { Component } from 'react';
+//import ReactDOM from 'react-dom';
+import './confirmationPage.css';
+import NavigationBar from './../NavigationBar/NavigationBar.js';
+import Footer        from './../Footer/Footer.js'
+//import searchicon from '../img/searchicon.png';
 
-	//this.handleChange = this.handleChange.bind(this);
-	//this.handleSubmit = this.handleSubmit.bind(this);
-    //}
+//import Crap from './'
 
-    //handleChange(event) {
-	//this.setState({value: event.target.value});
-    //}
-
-    //handleSubmit(event) {
-	//alert('A name was submitted: ' + this.state.value);
-	//event.preventDefault();
-    //}
+class confirmationPage extends Component {
 
     render() {
-	return (
-		<form onSubmit={this.handleSubmit}>
-        <label>
-		Name:
-		<input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// ISBN:
-		// <input name="isbn" type="text" value={this.state.isbn} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Class ID:
-		// <input name="classID" type="text" value={this.state.classID} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Textbook Title:
-		// <input name="textbookTitle" type="text" value={this.state.textbookTitle} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Edition Number:
-		// <input name="editionNumber" type="number" value={this.state.editionNumber} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Price:
-		// <input name="price" type="number" value={this.state.price} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Email:
-		// <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Phone Number:
-		// <input name="phoneNumber" type="text" value={this.state.phoneNumber} onChange={this.handleChange} />
-  //       </label>
-        
-  //       <label>
-		// Venmo ID:
-		// <input name="venmoID" type="text" value={this.state.venmoID} onChange={this.handleChange} />
-  //       </label>
-        
-        
-  
-        
-        
-        
-//         <input type="submit" value="Submit" />
-//       </form>
-// 		);
-//     }
-// }
-
-class ConfirmLabel extends React.Component {
-  render() {
-  	return(
-    <label>
-    	Name {this.props.name}
-    </label>
-   );
+        return (
+        	<div>
+        		<div class="back">
+        			<a href="confirmationPage">&#60; BACK</a>
+        		</div>
+        		<div class="vertflex">
+			      	<div class="border">
+			      		<div class = "horizflex">
+					      	<div class="vertflex">
+					      		<h3 class="rightalign">Book Title</h3>
+					      		<h3 class="rightalign">Author</h3>
+					      		<h3 class="rightalign">Version</h3>
+					      		<h3 class="rightalign">Condition</h3>
+					      		<h3 class="rightalign">Textbook ID</h3>
+					      	</div>
+					      	<div class="vertflex">
+					      		<h5 id="booktitle">Intro to advanced descriteid adnc</h5>
+					      		<h5 id="author">Miller, Philip</h5>
+					      		<h5 id="version">5</h5>
+					      		<h5 id="condition">Great</h5>
+					      		<h5 id="textbookid">1234-567-89</h5>
+					      	</div>
+				        </div>
+		  	        </div>
+		  			<div class="horizflex">
+		  				<button id= "continuebutton"><a href="BuyCart">Continue</a></button>
+		  			</div>
+		  	    </div>
+	        </div>
+        );
+    }
 }
-      
 
+class App extends Component {
+	render() {
+		return(
+			<div>
+				<div className="BodyWrapper">
+					<NavigationBar />
+					<div className="confirmationPage" style={{backgroundColor: '#fff'}}>
+						<confirmationPage
+                            />
+			 		</div>
+                </div>
+            <Footer position="relative" />
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(
-		<NameForm />,
-		document.getElementById('root')
-		);
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
+<<<<<<< Updated upstream
 import Sell from './Components/Sell/Sell';
+=======
+import confirmationPage from './Components/Sell/confirmationPage';
+
+>>>>>>> Stashed changes
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Route } from 'react-router-dom'
@@ -16,8 +21,13 @@ ReactDOM.render(
 	    <Router>
 	      <Route exact path='/about' component={About}/>
 	    </Router>
+<<<<<<< Updated upstream
 		<Router>
 	      <Route exact path='/sell' component={Sell}/>
+=======
+	    <Router>
+	      <Route exact path='/confirmation' component={confirmationPage}/>
+>>>>>>> Stashed changes
 	    </Router>
     </div>, document.getElementById('root'));
 registerServiceWorker();
