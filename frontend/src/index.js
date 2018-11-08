33e302dcb2b66b38c13cb253be382e27a4af7e24
Bianca <bianca.capretta@gmail.com>
2018-11-08 00:12:53 -0500

    Added back in UserHome and AdminHome; adjusted navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Components/Home/Home';
+import UserHome from './Components/UserHome/UserHome/UserHome';
+import AdminHome from './Components/AdminHome/AdminHome/AdminHome';
 import About from './Components/About/About';
-import Sell from './Components/Sell/Sell';
+import PreSell from './Components/Sell/Sell';
 import BuyFind from './Components/Buy/BuyFind/BuyFind';
 import BuyCart from './Components/Buy/BuyCart/BuyCart';
 import BuyAddtoCart from './Components/Buy/BuyAddtoCart/BuyAddtoCart';
@@ -14,20 +15,23 @@ import { Route } from 'react-router-dom'
 ReactDOM.render(
 	<div className="BodyWrapper">
 	    <Router>
-	      <Route exact path='/' component={Home}/>
+	      <Route exact path='/' component={UserHome}/>
+	    </Router>
+		<Router>
+	      <Route exact path='/adminhome' component={AdminHome}/>
 	    </Router>
 	    <Router>
 	      <Route exact path='/about' component={About}/>
 	    </Router>
 		<Router>
-	      <Route exact path='/sell' component={Sell}/>
+	      <Route exact path='/presell' component={PreSell}/>
 	    </Router>
 		<Router>
 	      <Route exact path='/buy' component={BuyFind}/>
 	    </Router>
 	    <Router>
 	      <Route exact path='/buyAddtoCart' component={BuyAddtoCart}/>
-	    </Router>	    
+	    </Router>
 		<Router>
 	      <Route exact path='/buyCart' component={BuyCart}/>
 	    </Router>
