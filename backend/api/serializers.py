532d2f95bf407112bbe60ebb9ddb033c644a80e9
Juliet Yue <julietyue@Juliets-MacBook-Pro-3.local>
2018-11-19 12:09:38 -0500

    add user table

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import *
 
+
 class SellSerializer(serializers.ModelSerializer):
-
 	class Meta:
-
 		model = Textbook
-		fields = ('post_id', )
-		# , 'isbn','subject','class_id','title','edition','price')
+		fields = ('post_id', 'date_added', 'subject','isbn','class_id','title',
+			      'edition','price','paid_type','date_sold','buyer_id')
+
