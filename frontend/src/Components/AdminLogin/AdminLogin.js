e6a16072cc0a41e59f2a67f4e9b98e20650d1e2a
Jake Owen <jakeowen1@gmail.com>
2018-11-26 12:38:04 -0500

    finished admin login page but styling is not 100%

--- frontend/src/Components/AdminLogin/AdminLogin.js
+++ frontend/src/Components/AdminLogin/AdminLogin.js
@@ -4,6 +4,7 @@ import './AdminLogin.css';
 import NavigationBar from '../NavigationBar/NavigationBar.js';
 import Footer from '../Footer/Footer.js'
 import history from '../../history';
+import logo from '../../assets/logo.png';
 
 
 class AdminLogin extends Component {
@@ -21,8 +22,7 @@ class AdminLogin extends Component {
 
   handleSubmit(event) {
     if(this.state.value == "jumbo"){
-    	//alert('Correct password')
-    	history.push('/adminhome');
+    	history.push('/AdminHome');
     	window.location.reload()
     }
     event.preventDefault();
@@ -34,12 +34,12 @@ class AdminLogin extends Component {
 	        <label>
 	        	Password:
 	        	<input type="password" value={this.state.value} onChange={this.handleChange} 
-	        		style={{width: 600, height: 50, fontSize: 40, borderRadius: 10, position: 'fixed', top: 475, right: 100}} 
+	        		style={{width: 600, height: 50, fontSize: 40, borderRadius: 10, position: 'fixed', top: 425, right: 100}} 
 	        	/>
 	       </label>
 	        	<input type="submit" value="Submit" 
 	        		style={{width: 400, height: 100, backgroundColor:'#4A90E3', fontSize: 40, color: 'white', borderRadius: 10, 
-	        		position: 'fixed', top: 600, right: 100}}
+	        		position: 'fixed', top: 550, right: 100}}
 	        	/>
 	       </form>
 		);
@@ -51,17 +51,21 @@ class AdminLogin extends Component {
 class App extends Component{
 	render() {
 		return (
-			<div className="Login" style={{backgroundColor: 'black', width: 1500, height: 1000}}>
-				<p style={{position: 'fixed', top: 200, right: 300}}>
+			<div className="Login" style={{backgroundColor: 'black', opacity: .85, width: 1500, height: 1000}}>
+				<p style={{position: 'fixed', top: 175, right: 300}}>
 							Please login using the administrators password
 				</p>
-				<p style={{position: 'fixed', top: 275, right: 230}}>
+				<p style={{position: 'fixed', top: 225, right: 230}}>
 							If you have forgotten the password, please ask another <br />administrator for the passcode
 				</p>
-				<p style={{position: 'fixed', top: 400, right: 615}}>
+				<p style={{position: 'fixed', top: 350, right: 615}}>
 							Passcode:
 				</p>
+				<p style={{position: 'fixed', top: 750, right: 650}}>
+							Copyright TEN 2018
+				</p>
 				<AdminLogin/>
+				<img src={logo} style={{position: 'fixed', top: 75, right: 800, height: 725, width: 600}}/>
 			</div>	
 		);
 	}
