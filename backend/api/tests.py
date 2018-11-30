0e685386f2426d44f3781b5443c2f7e68fcbcb3f
Bianca <bianca.capretta@gmail.com>
2018-11-30 17:46:11 -0500

    Added api.models

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Textbook
+from api.models import Textbook
 
 class TextbookTests(APITestCase):
 
