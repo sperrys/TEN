85ba0e7b4bb5e9436891b90fa95cd4b7b33dbf14
Bianca <bianca.capretta@gmail.com>
2018-11-30 17:18:22 -0500

    Added travis.py file

---
+++ backend/src/settings/travis.py
@@ -0,0 +1,14 @@
+import os
+import base
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
