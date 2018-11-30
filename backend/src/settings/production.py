a6df6b6e498f54702558e1899e18262e6a6ccba8
Bianca <bianca.capretta@gmail.com>
2018-11-30 18:16:08 -0500

    Squashin bugs

--- backend/src/settings/production.py
+++ backend/src/settings/production.py
@@ -16,7 +16,7 @@ CACHES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = confic('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
